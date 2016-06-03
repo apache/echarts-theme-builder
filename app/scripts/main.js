41ce1272a7b64721c6685d0836f85588b867faf6
Ovilia <zwl.sjtu@gmail.com>
2016-06-03 17:47:00 +0800

    use vue and bs color picker to select color

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,30 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.color').colorpicker({
+    align: 'left'
+  }).on('changeColor', function(e) {
+    console.log(vm);
+    var rgba = e.color.toRGB();
+    var rgbaStr = 'rgba(' + rgba.r + ', ' + rgba.g + ', ' + rgba.b + ', '
+      + rgba.a + ')';
+    vm.changeColor(rgbaStr);
+  });
+});
+
+var vm = new Vue({
+  el: '#acc-theme-color',
+
+  data: {
+    colors: ['#ffcc00', '#ccff00', '#00ffcc', '#00ccff'],
+    selectedColorId: null
+  },
+
+  methods: {
+    changeColor: function(color) {
+      this.$set('colors[' + this.selectedColorId + ']', color);
+    },
+
+    selectColor: function(id) {
+      this.$set('selectedColorId', id);
+    }
+  }
+});

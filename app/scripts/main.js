b04f9a323de5c96a512e8575e836c35d5ec93296
Ovilia <zwl.sjtu@gmail.com>
2016-06-06 15:30:00 +0800

    update echarts theme with color picker

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,9 +6,13 @@ $(document).ready(function() {
       if (isRootEvent) {
         isRootEvent = false;
         $(e.currentTarget).children('input').trigger('change', false);
+        updateCharts();
         isRootEvent = true;
       }
     });
+
+  // init charts
+  updateCharts();
 });
 
 
@@ -19,13 +23,68 @@ var vm = new Vue({
   data: {
     theme: {
       backgroundColor: '#ccc',
+      titleColor: '#333',
+      textColor: '#999',
       color: ['#ffcc00', '#ccff00', '#00ffcc', '#00ccff']
-    }
+    },
+    charts: (function() {
+      var options = getOptions();
+      var charts = [];
+      // init charts object
+      for (var cid = 0; cid < options.length; ++cid) {
+        charts.push({
+          option: JSON.stringify(options[cid]),
+          chart: null
+        });
+      }
+      return charts;
+    })()
   },
 
-  methods: {
-    aaa: function() {
-      console.log(arguments);
-    }
-  }
+  methods: {}
 });
+
+
+
+function getOptions() {
+  return [{
+    title: {
+      text: '折线图'
+    }
+  }, {
+    title: {
+      text: '柱状图'
+    }
+  }, {
+    title: {
+      text: '饼图'
+    }
+  }];
+}
+
+function getTheme() {
+  return {
+    color: vm.theme.color,
+    backgroundColor: vm.theme.backgroundColor,
+    textStyle: {
+      color: vm.theme.textColor
+    },
+    title: {
+      textStyle: {
+        color: vm.theme.titleColor
+      }
+    }
+  };
+}
+
+function updateCharts() {
+  console.log('updateCharts');
+  echarts.registerTheme('customed', getTheme());
+
+  // re-draw charts
+  $('.ec-panel').each(function() {
+    var chart = echarts.init(this, 'customed');
+    var option = JSON.parse($(this).attr('ec-option'));
+    chart.setOption(option);
+  });
+}

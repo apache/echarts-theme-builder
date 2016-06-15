17c6db951047906f74841dbeca730ab51c7e4e86
Ovilia <zwl.sjtu@gmail.com>
2016-06-15 19:22:50 +0800

    heatmap and treemap

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -544,6 +544,104 @@ function getOptions(vm) {
         {name: '澳门', value: 800}
       ]
     }]
+  }, {
+    title: {
+      text: '热力图'
+    },
+    xAxis: {
+      type: 'category',
+      data: ['12a', '1a', '2a', '3a', '4a', '5a', '6a',
+        '7a', '8a', '9a','10a','11a',
+        '12p', '1p', '2p', '3p', '4p', '5p',
+        '6p', '7p', '8p', '9p', '10p', '11p']
+    },
+    yAxis: {
+      type: 'category',
+      data: ['周六', '周五', '周四', '周三', '周二', '周一', '周日']
+    },
+    visualMap: {
+      min: 1,
+      max: 10,
+      calculable: true
+    },
+    grid: {
+      left: 90,
+      right: 20,
+      top: 40,
+      bottom: 0
+    },
+    series: [{
+      name: '热度',
+      type: 'heatmap',
+      data: (function() {
+        var data = [[0,0,5],[0,1,1],[0,2,3],[0,3,2],[0,4,1],[0,5,4],[0,6,6],
+          [0,7,6],[0,8,4],[0,9,7],[0,10,0],[0,11,2],[1,0,7],[1,1,0],[1,2,0],
+          [1,3,0],[1,4,5],[1,5,7],[1,6,9],[1,7,4],[1,8,7],[1,9,9],[1,10,5],
+          [1,11,2],[2,0,7],[2,1,6],[2,2,5],[2,3,5],[2,4,9],[2,5,8],[2,6,0],
+          [2,7,0],[2,8,0],[2,9,4],[2,10,3],[2,11,2],[3,0,7],[3,1,3],[3,2,0],
+          [3,3,0],[3,4,3],[3,5,0],[3,6,4],[3,7,5],[3,8,1],[3,9,3],[3,10,5],
+          [3,11,4],[3,12,7],[4,0,6],[4,1,3],[4,2,6],[4,3,0],[4,4,5],[4,5,1],
+          [4,6,0],[4,7,0],[4,8,5],[4,9,2],[4,10,4],[4,11,4],[5,0,2],[5,1,1],
+          [5,2,0],[5,3,3],[5,4,8],[5,5,4],[5,6,6],[5,7,0],[5,8,2],[5,9,0],
+          [5,10,4],[5,11,1],[6,0,1],[6,1,0],[6,2,8],[6,3,6],[6,4,2],[6,5,0],
+          [6,6,0],[6,7,8],[6,8,5],[6,9,10],[6,10,1],[6,11,0]];
+        return data.map(function (item) {
+          return [item[1], item[0], item[2] || '-'];
+        });
+      })(),
+      label: {
+        normal: {
+          show: true
+        }
+      }
+    }]
+  }, {
+    title: {
+      text: '树图'
+    },
+    series: [{
+      type: 'treemap',
+      label: {
+        show: true,
+        formatter: '{b}'
+      },
+      data: (function() {
+        var data = [];
+        for (var i = 0; i < groupCnt; ++i) {
+          var children = [];
+          var cSum = 0;
+          for (var c = 0, clen = Math.ceil(Math.random() * 5); c < clen; ++c) {
+            var grand = [];
+            var gSum = 0;
+            for (var g = 0, glen = Math.ceil(Math.random() * 3); g < glen;
+              ++g) {
+              var v = Math.ceil(Math.random() * 100);
+              grand.push({
+                value: v,
+                name: (i + 1) + '-' + (c + 1) + '-' + (g + 1),
+                path: (i + 1) + '/' + (i + 1) + '-' + (c + 1) + '/' +
+                  (i + 1) + '-' + (c + 1) + '-' + (g + 1)
+              });
+              gSum += v;
+            }
+            children.push({
+              value: gSum,
+              name: (i + 1) + '-' + (c + 1),
+              path: (i + 1) + '/' + (i + 1) + '-' + (c + 1),
+              children: grand
+            });
+            cSum += gSum;
+          }
+          data.push({
+            value: cSum,
+            name: '' + (i + 1),
+            path: '' + (i + 1),
+            children: children
+          });
+        }
+        return data;
+      })()
+    }]
   }];
 
   for (var i = 0; i < options.length; ++i) {

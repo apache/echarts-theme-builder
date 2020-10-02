01fe2305b98882022fa516781bf4cc57d26ad4bf
plainheart <yhen@all-my-life.cn>
2020-10-02 21:44:24 +0800

    fix: fix wrong label structure in graph series.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -519,11 +519,7 @@ function getTheme(isToExport) {
         color: vm.theme.markTextColor
       };
       style.lineStyle = isFlattern ? lineStyle : { normal: lineStyle };
-      style.label = isFlattern ? {
-        textStyle: textStyle
-      } : {
-        normal: textStyle
-      };
+      style.label = isFlattern ? textStyle : { normal: textStyle };
       var itemStyle = isFlattern ? style.itemStyle : style.itemStyle.normal;
       itemStyle.borderWidth = vm.theme.borderWidth;
       itemStyle.borderColor = vm.theme.borderColor;

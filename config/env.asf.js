e80ac0a4ce3b0c9b52c6deb1912034453f32e15d
pissang <bm2736892@gmail.com>
2021-01-20 20:55:52 +0800

    update build target dir

--- config/env.asf.js
+++ config/env.asf.js
@@ -1,6 +1,6 @@
 const path = require('path');
 
 module.exports = {
-    releaseDestDir: path.resolve(__dirname, '../../echarts-website'),
+    releaseDestDir: path.resolve(__dirname, '../../echarts-website/v4'),
     ecWWWGeneratedDir: path.resolve(__dirname, '../../echarts-www/_generated')
 };

7d80a82d80827121944649c3a0efde73a24ceedc
Ovilia <zwl.sjtu@gmail.com>
2021-04-09 14:08:14 +0800

    core: remove all

--- config/env.asf.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    releaseDestDir: path.resolve(__dirname, '../../echarts-website'),
-    ecWWWGeneratedDir: path.resolve(__dirname, '../../echarts-www/_generated')
-};

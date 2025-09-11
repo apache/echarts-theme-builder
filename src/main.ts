4e44127488b53641da43665877abc03b0c34f58b
Ovilia <zwl.sjtu@gmail.com>
2025-09-11 15:21:53 +0800

    feat: update theme panel

--- src/main.ts
+++ src/main.ts
@@ -14,8 +14,10 @@ import {
 import 'vant/lib/index.css'
 import './style.css'
 import App from './App.vue'
+import i18n from './i18n'
 
 const app = createApp(App)
+app.use(i18n)
 app.use(Col)
 app.use(Row)
 app.use(Collapse)
